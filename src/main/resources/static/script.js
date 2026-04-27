90fcd0a825000777956b9ee4fd2c0cc9aa8d6015
shaharebhavesh <shaharebhavesh@gmail.com>
2026-04-27 15:15:34 +0530

    fix nav bar brand name

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -32,23 +32,34 @@ window.addEventListener("load", () => {
         // Step 2: animate
         setTimeout(() => {
             el.classList.add("active");
-        }, 150 + (index * 120));
+        }, 200 + (index * 120));
 
     });
 
 });
 
 /* ===== NAVBAR SCROLL EFFECT ===== */
+//window.addEventListener("scroll", () => {
+//    const nav = document.querySelector(".navbar");
+//
+//    if (window.scrollY > 50) {
+//        nav.style.padding = "10px 40px";
+//        nav.style.boxShadow = "0 8px 25px rgba(0,0,0,0.1)";
+//    } else {
+//        nav.style.padding = "14px 50px";
+//        nav.style.boxShadow = "0 4px 15px rgba(0,0,0,0.05)";
+//    }
+//});
+
 window.addEventListener("scroll", () => {
     const nav = document.querySelector(".navbar");
 
     if (window.scrollY > 50) {
-        nav.style.padding = "10px 40px";
-        nav.style.boxShadow = "0 8px 25px rgba(0,0,0,0.1)";
+        nav.classList.add("scrolled");
     } else {
-        nav.style.padding = "14px 50px";
-        nav.style.boxShadow = "0 4px 15px rgba(0,0,0,0.05)";
+        nav.classList.remove("scrolled");
     }
 });
+
 
 console.log("JS WORKING");
