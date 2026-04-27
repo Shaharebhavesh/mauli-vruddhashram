529d5c3505f7dacb2cf2a8fc29c2eda4ce9c5343
shaharebhavesh <shaharebhavesh@gmail.com>
2026-04-27 16:49:53 +0530

    fix nav bar brand name

--- src/main/java/com/mauli/mauli_vruddhashram/controller/PageController.java
+++ src/main/java/com/mauli/mauli_vruddhashram/controller/PageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "index";
     }
