cea45096e3086da622f0a012dc8c2a4f8b284152
shaharebhavesh <shaharebhavesh@gmail.com>
2026-06-10 15:47:25 +0530

    remove mysql and jpa dependency and preparing for render deployment

--- src/main/java/com/mauli/mauli_vruddhashram/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mauli.mauli_vruddhashram.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mauli.mauli_vruddhashram.model.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
